4a30a2ef93e8f1accd464a9f4fefcea2ea14ef4f
Ajnbro <50883410+Ajnbro@users.noreply.github.com>
2022-04-05 22:04:03 -0400

    Added tests for fill_local_version_list

--- tests/unit/saltenv/test_ops.py
+++ tests/unit/saltenv/test_ops.py
@@ -1,20 +1,9 @@
 import mock
-from pathlib import Path
+from pathlib import Path, PosixPath
 
-"""
-async def local_version_list(hub, **kwargs):
-    '''
-    This is the entrypoint for the async code in your project
-    '''
-    ret = {}
-    versions_dir = Path(hub.OPT.saltenv.saltenv_dir) / "versions"
-    if versions_dir.exists():
-        ret = versions_dir.glob("salt-*")
-        ret = {ver.name.replace("salt-", ""): ver for ver in ret}
-    return ret
-"""
 
 async def test_local_version_list_nonexistent_dir(mock_hub, hub):
+    # Link the real function to the mock_hub
     mock_hub.saltenv.ops.local_version_list = hub.saltenv.ops.local_version_list
     mock_hub.OPT.saltenv.saltenv_dir = "nonexistent_testing_dir"
     actual = await mock_hub.saltenv.ops.local_version_list()
@@ -23,6 +12,7 @@ async def test_local_version_list_nonexistent_dir(mock_hub, hub):
 
 
 async def test_local_version_list_existent_dir(mock_hub, hub, tmp_path):
+    # Link the real function to the mock_hub    
     mock_hub.saltenv.ops.local_version_list = hub.saltenv.ops.local_version_list
     mock_hub.OPT.saltenv.saltenv_dir = tmp_path
     
@@ -36,7 +26,6 @@ async def test_local_version_list_existent_dir(mock_hub, hub, tmp_path):
     assert actual == expected
 
     # Confirm that the return directory is correct when the directory contains valid and invalid version files
-
     # Create valid version files that will get picked up by the function 
     valid_name_1 = "salt-3001"
     valid_path_1 = mocked_versions_dir / valid_name_1
@@ -62,5 +51,36 @@ async def test_local_version_list_existent_dir(mock_hub, hub, tmp_path):
     # Compare the actual and expected results
     expected = {valid_name_1.replace("salt-", ""): Path(valid_path_1), valid_name_2.replace("salt-", ""): Path(valid_path_2)}
     actual = await mock_hub.saltenv.ops.local_version_list()
-    
     assert expected == actual
+
+
+async def test_fill_local_version_list(mock_hub, hub):
+    # Link the real function to the mock_hub
+    mock_hub.saltenv.ops.fill_local_version_list = hub.saltenv.ops.fill_local_version_list
+
+    # Default LOCAL_VERSIONS to {}    
+    mock_hub.saltenv.ops.LOCAL_VERSIONS = {}
+
+    # Set the mocked return value of local_version_list to be {}
+    # and confirm that the local_version_list value does not change
+    mock_hub.saltenv.ops.local_version_list.return_value = {}
+    
+    # Call fill_local_version_list
+    await mock_hub.saltenv.ops.fill_local_version_list()
+    
+    # Confirm the value of LOCAL_VERSIONS matches our expected results
+    expected = {}
+    assert mock_hub.saltenv.ops.LOCAL_VERSIONS == expected
+ 
+    # Now set the mocked return value to contain an element. This dictionary
+    # with one element will be returned by local_version_list and stored
+    # within the LOCAL_VERSIONS dict
+    mock_hub.saltenv.ops.local_version_list.return_value = {"salt-3001": Path('mocked/path/salt-3001')}
+    
+    # Call fill_local_version_list to populate the LOCAL_VERSIONS dict
+    await mock_hub.saltenv.ops.fill_local_version_list()
+
+    # Confirm the value of LOCAL_VERSION matches our expected results
+    expected = {"salt-3001": Path('mocked/path/salt-3001')}
+    assert mock_hub.saltenv.ops.LOCAL_VERSIONS == expected
+    
