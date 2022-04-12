cfd8e1a403d333ff849538fc84d1567833986a30
Ajnbro <50883410+Ajnbro@users.noreply.github.com>
2022-04-11 21:53:32 -0400

    Stashing progress

--- tests/unit/saltenv/cli/test_list.py
+++ tests/unit/saltenv/cli/test_list.py
@@ -20,32 +20,28 @@ async def list_(hub, **kwargs):
 """
 
 
-async def test_list(mock_hub, hub):
-    # Link the function to the mock_hub
-    mock_hub.saltenv.ops.fill_local_version_list = hub.saltenv.ops.fill_local_version_list
+async def test_list1(mock_hub, hub):
+    """
+    SCENARIO #1:
+    - No local versions
+    - No current version
+    """
+    pass
 
-    # Default LOCAL_VERSIONS to {}
-    mock_hub.saltenv.ops.LOCAL_VERSIONS = {}
 
-    # Set the mocked return value of local_version_list to be {}
-    # and confirm that the local_version_list value does not change
-    mock_hub.saltenv.ops.local_version_list.return_value = {}
+async def test_list2(mock_hub, hub):
+    """
+    SCENARIO #2:
+    - 2 local versions
+    - No current version
+    """
+    pass
 
-    # Call fill_local_version_list
-    await mock_hub.saltenv.ops.fill_local_version_list()
 
-    # Confirm the value of LOCAL_VERSIONS matches our expected results
-    expected = {}
-    assert mock_hub.saltenv.ops.LOCAL_VERSIONS == expected
-
-    # Now set the mocked return value to contain an element. This dictionary
-    # with one element will be returned by local_version_list and stored
-    # within the LOCAL_VERSIONS dict
-    mock_hub.saltenv.ops.local_version_list.return_value = {"3001": Path("mocked/path/salt-3001")}
-
-    # Call fill_local_version_list to populate the LOCAL_VERSIONS dict
-    await mock_hub.saltenv.ops.fill_local_version_list()
-
-    # Confirm the value of LOCAL_VERSIONS matches our expected results
-    expected = {"3001": Path("mocked/path/salt-3001")}
-    assert mock_hub.saltenv.ops.LOCAL_VERSIONS == expected
+async def test_list3(mock_hub, hub):
+    """
+    SCENARIO #3:
+    - 2 local versions
+    - A current version
+    """
+    pass
