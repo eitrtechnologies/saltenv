99e982b92cadb796a9abfaf233887ec5823174c5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 15:31:52 +0000

    Adding tarfile member sanitization to extractall()

--- saltenv/saltenv/ops.py
+++ saltenv/saltenv/ops.py
@@ -137,7 +137,26 @@ async def download_version(hub, version, **kwargs):
                     print("Processing tarball...")
                     if tarfile.is_tarfile(outfile):
                         with tarfile.open(outfile) as tar_source:
-                            tar_source.extractall(versions_dir)
+                            def is_within_directory(directory, target):
+                                
+                                abs_directory = os.path.abspath(directory)
+                                abs_target = os.path.abspath(target)
+                            
+                                prefix = os.path.commonprefix([abs_directory, abs_target])
+                                
+                                return prefix == abs_directory
+                            
+                            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                            
+                                for member in tar.getmembers():
+                                    member_path = os.path.join(path, member.name)
+                                    if not is_within_directory(path, member_path):
+                                        raise Exception("Attempted Path Traversal in Tar File")
+                            
+                                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                                
+                            
+                            safe_extract(tar_source, versions_dir)
                         if salt_bin_in.exists():
                             salt_bin_in.rename(salt_bin_out)
                         ret = salt_bin_out.exists()
